928d2f890eef1d9e389634d0776591ae49aacd87
NikitaKovyazov <92822645+NikitaKovyazov@users.noreply.github.com>
2022-05-24 15:31:52 +0300

    Create n4.js

---
+++ n4.js
@@ -0,0 +1,10 @@
+const  lastNumber = 20;
+let multiplicationResult = 0;
+for(let i = 0;i <= lastNumber;i++)
+  {
+    if(i%2 == 0)
+      {
+        multiplicationResult += i;
+      }
+  }
+alert(multiplicationResult);
